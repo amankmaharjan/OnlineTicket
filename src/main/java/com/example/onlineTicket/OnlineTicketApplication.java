1c7e7e468584e24484150053168d6591c13ed4ee
aman <amanht3@gmail.com>
2017-07-07 16:39:32 +0545

    first commit

---
+++ src/main/java/com/example/onlineTicket/OnlineTicketApplication.java
@@ -0,0 +1,12 @@
+package com.example.onlineTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTicketApplication.class, args);
+	}
+}
